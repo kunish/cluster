d9fe74ce80b4d782e025cc84604cb7b3cddcfdc9
shikun <kunish.butt@gmail.com>
2021-08-04 03:21:01 +0800

    feat: add helmfile

--- setup.sh
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/env bash
-
-# metallb
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/master/manifests/namespace.yaml
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/master/manifests/metallb.yaml
-kubectl apply -f metallb
