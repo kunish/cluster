3da9dc2d02f23ba866350b81c74b3246f481a50a
kunish <kunish.butt@gmail.com>
2021-11-17 23:25:49 +0800

    feat: add k3sup-provision.fish script

---
+++ bin/k3sup-provision.fish
@@ -0,0 +1,18 @@
+set -x ssh_user shikun
+set -x ssh_key $HOME/.ssh/home_id_rsa
+set -x server_ip 10.10.0.50
+
+k3sup install \
+    --cluster \
+    --ip $server_ip --user $ssh_user --ssh-key $ssh_key \
+    --merge --local-path $HOME/.kube/kubeconfig.yml --context k3s \
+    --k3s-channel latest \
+    --k3s-extra-args '--disable servicelb traefik local-storage' \
+    --print-command
+
+for host in 10.10.0.51 10.10.0.52 10.10.0.53
+    k3sup join \
+        --server-ip $server_ip --server-user $ssh_user --ip $host --user $ssh_user --ssh-key $ssh_key \
+        --k3s-channel latest \
+        --print-command
+end
