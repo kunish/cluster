87a78243aa45a839ef2354977895852c43cd90d8
shikun <shikun@huohua.cn>
2022-05-05 02:20:24 +0800

    feat: add playbook

--- main.tf
+++ main.tf
@@ -29,16 +29,6 @@ resource "helm_release" "nfs-subdir-external-provisioner" {
   values            = [file("values/nfs-subdir-external-provisioner.yml")]
 }
 
-resource "helm_release" "minio" {
-  name              = "minio"
-  repository        = "https://charts.bitnami.com/bitnami"
-  chart             = "minio"
-  namespace         = "minio"
-  create_namespace  = true
-  dependency_update = true
-  values            = [file("values/minio.yml")]
-}
-
 resource "helm_release" "prometheus" {
   name              = "prometheus"
   repository        = "https://prometheus-community.github.io/helm-charts"
